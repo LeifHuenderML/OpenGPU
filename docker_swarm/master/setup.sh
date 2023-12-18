a0540a1985ba0bc159e96320ca94419e744b3e4a
Leif Huender <leifhuenderai@gmail.com>
2023-12-17 17:31:51 -0800

    pivoting from kubernetes to using docker swarm because of its simplicity

---
+++ docker_swarm/master/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to get the primary IP address
+get_primary_ip() {
+    # This command gets the primary IP address assigned to the first non-loopback network interface
+    ip addr show scope global | awk '/inet /{print $2}' | cut -d/ -f1 | head -n 1
+}
+
+# Function to check the exit status of the last command executed
+check_status() {
+    if [ $? -ne 0 ]; then
+        echo "An error occurred: $1"
+        exit 1
+    fi
+}
+
+# Initialize Docker Swarm
+init_swarm() {
+    local ip_addr=$(get_primary_ip)
+    if [[ -z "$ip_addr" ]]; then
+        echo "Unable to determine the primary IP address."
+        exit 1
+    fi
+
+    echo "Initializing Docker Swarm with IP: $ip_addr"
+    sudo docker swarm init --advertise-addr "$ip_addr"
+    check_status "Failed to initialize Docker Swarm."
+}
+
+# Main function
+main() {
+    init_swarm
+}
+
+# Execute the main function
+main
--- kubernetes/k8s-master/setup.sh
+++ kubernetes/k8s-master/setup.sh
@@ -1,29 +1,95 @@
-#makes the node distinguishable as a master node
-sudo hostnamectl set-hostname kubemaster
-#update and install kubernetes helper packages
-sudo apt-get update && sudo apt-get install -y apt-transport-httpsgnupg2
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo “deb https://apt.kubernetes.io/ kubernetes-xenial main” | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+#!/bin/bash
+
+# Update and Upgrade
+sudo apt update -y && sudo apt upgrade -y
+
+# Install dependencies
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+# Create directory for keyrings if it doesn't exist
+sudo mkdir -p /etc/apt/keyrings
+
+# Download and add Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Set correct permissions for the GPG key
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the Docker repository to Apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists
 sudo apt-get update
-#installs kubernetes and packages on master node
-sudo apt-get install -y kubectl kubeadm kubelet kubelet kuberneses-cni docker.io
-#get docker running
-sudo systemctl start docker
-sudo systemctl enable docker
-#get k8s running
-sudo systemctl start kubelet
-sudo systemctl enable kubelet
-#add current user to the docker group so docker commands can be run with root priveleges
-sudo usermod -aG docker $user
-newgrp docker
-#enable iptables to see bridged traffic 
-cat << EOF | sudo tee /etc/sysctl.d/k8s/conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+
+# Install Docker packages
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable and start Docker service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+# Load required modules for Kubernetes
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
 EOF
-sudo systemctl - system
-#pull k8s packages
-sudo kubeadm config images pull
-#let kubeadm prepare the environment setting the private ip to the master nodes private ip
-private_ip=&(hostname -I | awk '{print &1}')
-sudo kubeadm init - apiserver-advertise-address=<&private_ip> - pod-network-cidr-172.16.0.0/16
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Sysctl parameters required by Kubernetes, persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Configure Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Reload and restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# Add Kubernetes repository
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update package lists
+sudo apt-get update
+
+# Install Kubernetes components
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Set hostname
+sudo hostnamectl set-hostname kubemaster
+
+# Restart containerd
+sudo systemctl restart containerd
+
+#turn off swap
+sudo swapoff -a
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+#set the directory for kubernetes
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/custom-resources.yaml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
