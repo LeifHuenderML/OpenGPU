2c5e49e3d13412cf66dd1896c24d9f996148624c
Leif Huender <leifhuenderai@gmail.com>
2023-12-19 11:57:33 -0800

    added nvidia container toolkit to setup script for worker nodes

--- docker_swarm/worker/setup.sh
+++ docker_swarm/worker/setup.sh
@@ -9,7 +9,7 @@ echo "Installing necessary packages..."
 sudo apt-get install -y jq build-essential
 
 echo "Retrieving Docker Swarm join command..."
-response=$(curl -s http://192.168.1.192:5000/join_swarm || { echo "Failed to connect to the server."; exit 1; })
+response=$(curl -s http://192.168.1.192:5000/join_swarm || { echo "Failed to connect to the api server."; exit 1; })
 join_command=$(echo $response | jq -r .command)
 
 if [ -n "$join_command" ]; then
@@ -20,23 +20,27 @@ else
     exit 1
 fi
 
-echo "Checking for NVIDIA GPU..."
-if lspci | grep -qi nvidia; then
-    echo "NVIDIA GPU detected. Proceeding with driver setup..."
-    sudo add-apt-repository ppa:graphics-drivers/ppa
-    sudo apt-get update
-    ubuntu-drivers devices
-    sudo ubuntu-drivers autoinstall
+echo "Setting up NVIDIA drivers..."
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
 
-    echo "System needs to be rebooted to complete the installation."
-    read -p "Do you want to reboot now? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        sudo reboot
-    else
-        echo "Please reboot the system manually to apply changes."
-    fi
+echo "Installing NVIDIA Container Toolkit..."
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+
+echo "Restarting Docker to apply changes..."
+sudo systemctl restart docker
+
+echo "System needs to be rebooted to complete the installation."
+read -p "Do you want to reboot now? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo reboot
 else
-    echo "No NVIDIA GPU detected. Skipping driver installation."
+    echo "Please reboot the system manually to apply changes."
 fi
