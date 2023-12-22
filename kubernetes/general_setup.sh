ef2d665e17cf498aef673df85a99f64f3e50c359
Leif Huender <leifhuenderai@gmail.com>
2023-12-22 11:14:39 -0800

    cloned over Andrej Karpathys miniGPT to test the distributed gpu cluster with

--- kubernetes/general_setup.sh
+++ kubernetes/general_setup.sh
@@ -2,7 +2,7 @@
 #for giving most of the setup implementation found here
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -22,7 +22,7 @@ EOF
 
 sudo sysctl --system
 
-sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates 
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
