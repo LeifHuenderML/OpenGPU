c9a24bc09960c8c76f3b09f609417f4720e9f3ac
Leif Huender <leifhuenderai@gmail.com>
2023-12-21 19:01:57 -0800

    added credit to the setup scripts documentation

--- kubernetes/general_setup.sh
+++ kubernetes/general_setup.sh
@@ -1,3 +1,6 @@
+#credit goes to https://hbayraktar.medium.com/how-to-install-kubernetes-cluster-on-ubuntu-22-04-step-by-step-guide-7dbf7e8f5f99
+#for giving most of the setup implementation found here
+
 sudo apt update
 sudo apt upgrade
 
