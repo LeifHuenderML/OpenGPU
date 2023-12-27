654e399acd0bec0755ff94a7ad5b959975b56d50
Leif Huender <leifhuenderai@gmail.com>
2023-12-26 21:46:43 -0800

    started getting the scripts built in a way to automate the installation and testing process, added a system check for ram to determine to make the machine be setup either a file system server or apart of the training cluster

--- build.sh
+++ build.sh
@@ -6,20 +6,33 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Makes all scripts executable for openGPU repository
 find . -type f -exec chmod +x {} \;
 
-# Run general setup script
-./kubernetes/general_setup.sh
+# Determine resources of system
+# Get the available memory in megabytes
+available_ram=$(free -m | awk '/^Mem:/ {print $7}')
 
-# Prompt for node type
-echo "Is this computer intended to be a master or worker node? (enter 'm' for master or 'w' for worker)"
-read input
+# Define the threshold (4GB in MB)
+threshold=4096
 
-# Check input and run the appropriate script
-if [ "$input" == "m" ]; then
-    ./kubernetes/master/setup.sh
-elif [ "$input" == "w" ]; then
-    ./kubernetes/worker/setup.sh
+# Check if the available memory is greater than or equal to the threshold
+if [ "$available_ram" -ge "$threshold" ]; then
+    echo "Enough RAM available, adding this system to the training cluster."
+    # Run general setup script
+    ./kubernetes/general_setup.sh
+    # Prompt for node type
+    echo "Is this computer intended to be a master or worker node? (enter 'm' for master or 'w' for worker)"
+    read input
+
+    # Check input and run the appropriate script
+    if [ "$input" == "m" ]; then
+        ./kubernetes/master/setup.sh
+    elif [ "$input" == "w" ]; then
+        ./kubernetes/worker/setup.sh
+    else
+        echo "That was incorrect input. Try inputting a 'm' for master or 'w' for worker next time."
+        exit 1
+    fi
 else
-    echo "That was incorrect input. Try inputting a 'm' for master or 'w' for worker next time."
-    exit 1
+    echo "Not enough RAM, for the training cluster adding this machine to the distributed files system cluster."
+    ./glusterfs/setup/general_setup.sh 
 fi
 
