412aeeacfcddd8f6421d5ecd7a76350ba9d8c5c9
Leif Huender <leifhuenderai@gmail.com>
2023-12-30 19:04:20 -0800

    fixed bugs in the simple training script so that it should train now

--- docker/application/simple_model/train.py
+++ docker/application/simple_model/train.py
@@ -1,10 +1,17 @@
 import os
+import torch
 from torch import optim, nn
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from pytorch_lightning.strategies import DDPStrategy
+import socket
+import pickle
+
+hostname = socket.gethostname()
+rank = int(hostname[len("worker"):]) - 1
+os.environ['RANK'] = str(rank)
 
 # Assuming encoder and decoder are defined as before
 
@@ -29,11 +36,35 @@ class LitAutoEncoder(pl.LightningModule):
 
     def train_dataloader(self):
         # Data loader with distributed sampler for DDP
-        dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+        dataset = self.fetch_dataset()
         train_sampler = None
-        if self.use_ddp:
-            train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
         return DataLoader(dataset, batch_size=64, sampler=train_sampler)
+
+    def fetch_dataset(self):
+        data_path = '/data/mnist_dataset/mnist_dataset.pkl'
+        with open(data_path, 'rb') as file:
+            dataset = pickle.load(file)
+        return dataset
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.linear = nn.Linear(28 * 28, 128) 
+
+    def forward(self, x):
+        return self.linear(x)
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.linear = nn.Linear(128, 28 * 28) 
+
+    def forward(self, x):
+        return self.linear(x)
+
+encoder = Encoder()
+decoder = Decoder()
 
 # init the autoencoder
 autoencoder = LitAutoEncoder(encoder, decoder)
@@ -41,7 +72,6 @@ autoencoder = LitAutoEncoder(encoder, decoder)
 # Set up the trainer for DDP
 trainer = pl.Trainer(
     strategy=DDPStrategy(),  # Use DistributedDataParallel strategy
-    gpus=8,  # Number of GPUs (adjust as needed)
     limit_train_batches=100,
     max_epochs=1
 )
